dfdb692b87f4c298f62c7837bad499de74a39c27
OtsoNousiainen <nousianenotso@gmail.com>
2025-02-12 18:14:26 +0200

    joo

--- public/script.js
+++ public/script.js
@@ -1,18 +1,25 @@
 let BOARD_SIZE = 15
 let board; //kenttä tallennetaan tähän
-
-
-
+const cellSize = calculateCellSize();
 
 
 
 document.getElementById("new-game-btn").addEventListener('click', startGame);
+
+function calculateCellSize(){
+const screenSize = Math.min(window.innerWidth, innerHeight);
+const gameBoardSize = 0.95 * screenSize;
+return gameBoardSize / BOARD_SIZE;
+
+}
 
 function startGame(){
     document.getElementById('intro-screen').style.display = 'none';
     document.getElementById('game-screen').style.display = 'block';
 
     board = generateRandomBoard();
+
+    drawBoard(board);
 }
 
 function getCell(board, x, y) {
@@ -37,5 +44,28 @@ for(let y=0; y < BOARD_SIZE; y++){
 
 return newBoard;
 
+}
+
+function drawBoard(board){
+    const gameBoard = document.getElementById('game-board');
+
+    gameBoard.style.gridTemplateColumns = `repeat(${BOARD_SIZE}, 1fr)`;
+
+for(let y=0; y < BOARD_SIZE; y++){
+    for(let x=0; x < BOARD_SIZE; x++){
+        const cell = document.createElement('div');
+        cell.classList.add('cell')
+        cell.classList.add('wall');
+            cell.style.width = cellSize + "px";
+            cell.style.height = cellSize + "px";
+
+        if (getCell(board, x, y) === 'W'){
+            
+        }
+
+        gameBoard.appendChild(cell);
+
+    }
+}
 
 }
